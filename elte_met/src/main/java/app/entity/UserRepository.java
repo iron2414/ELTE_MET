8e7a515d7186a7ebe9d27f1646ef164f6f113fd7
iron2414 <iron2414@gmail.com>
2018-10-28 18:06:51 +0100

    unused import delete

--- elte_met/src/main/java/app/entity/UserRepository.java
+++ elte_met/src/main/java/app/entity/UserRepository.java
@@ -2,17 +2,12 @@ package app.entity;
 
 import com.querydsl.core.types.dsl.StringExpression;
 import com.querydsl.core.types.dsl.StringPath;
-import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.querydsl.binding.SingleValueBinding;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 
 @Repository
